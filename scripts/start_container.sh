2dbd0c3d5c417a2972e2ff6446cd45edf81bba1f
Ashutosh Bambal <92073828+ashubambal@users.noreply.github.com>
2023-08-17 11:38:33 +0530

    Update start_container.sh
    script modified

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker push softconsist/simple-python-flask-app
+docker push softconsist/simple-python-flask-app:tagname
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 softconsist/simple-python-flask-app
+docker run -d -p 5000:5000 softconsist/simple-python-flask-app:tagname
